89a1511660ea80a36da2ce059d1548bb6fd8a0f2
fmabece023 <fmabece023@student.wethinkcode.co.za>
2024-08-02 13:37:58 +0200

    Changes for data presentation

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import CSVReader from 'react-csv-reader';
 import logo from './logo.svg';
 import './App.css';
 import Dashboard from './components/Dashboard';
+import axios from 'axios';
 
 function App() {
   const [file, setFile] = useState([]);
+  const [recent, setRecent] = useState(1);
+  const [data, setData] = useState();
+  const [headers, setHeaders] = useState([]);
   // Map headers to desired headers
-  const headerMapping = {
-    'sales_date': 'sales_date',
-    'volume': 'volume',
-    'rel_promo_price': 'rel_promo_price',
-    'is_promo': 'is_promo',
-    'is_single_price_promo': 'is_single_price_promo',
-    'is_multibuy_promo': 'is_multibuy_promo',
-    'rsp': 'rsp',
-    'planned_promo_vol': 'planned_promo_vol',
-    'product_code': 'product_code',
-    'sale_date': 'sale_date',
-  };
+  // const headerMapping = {
+  //   'sales_date': 'sales_date',
+  //   'volume': 'volume',
+  //   'rel_promo_price': 'rel_promo_price',
+  //   'is_promo': 'is_promo',
+  //   'is_single_price_promo': 'is_single_price_promo',
+  //   'is_multibuy_promo': 'is_multibuy_promo',
+  //   'rsp': 'rsp',
+  //   'planned_promo_vol': 'planned_promo_vol',
+  //   'product_code': 'product_code',
+  //   'sale_date': 'sale_date',
+  // };
 //uncomment once csv created
 
   const papaparseOptions = {
@@ -26,15 +30,30 @@ function App() {
     dynamicTyping: true,
     skipEmptyLines: true,
     transformHeader: header => 
-      header
-        .toLowerCase()
-        .replace(/\W/g, '_')
+      header.toLowerCase().replace(/\W/g, '_')
   };
+
+  useEffect(() => {
+    if (data != null) {
+      handleForce(data)
+
+    }
+  }, [recent])
 
   function handleForce(data) {
     //cap data to read last 30
-    const cappedData = data.slice(-30);
+    const cappedData = data.slice(-(data.length * (recent/100)));
+    const csvHeaders = cappedData.length > 0 ? Object.keys(cappedData[0]) : [];
+    setHeaders(csvHeaders);
     setFile(cappedData);
+  }
+
+  const sendCSVtoServer = async (file) => {
+    await axios.post('https://nerd-biz-bot.vercel.app/csv', {
+      file
+    })
+    .then((res) => console.log(res))
+    .catch((err) => console.warn(err))
   }
 
   return (
@@ -46,7 +65,11 @@ function App() {
         <CSVReader
           cssClass="csv-reader-input"
           label="Select CSV with business data"
-          onFileLoaded={handleForce}
+          onFileLoaded={async(e) => {
+            handleForce(e);
+            setData(e);
+            // sendCSVtoServer(e);
+          }}
           onError={(error) => console.error(error)}
           parserOptions={papaparseOptions}
           inputId="CSVInput"
@@ -54,6 +77,14 @@ function App() {
           inputStyle={{ color: 'red' }}
         />
       </div>
+      <div className='d-flex flex-column justify-content-center align-items-center my-3'>
+        <label for="customRange1" class="form-label d-block mt-3">Recent data range</label>
+        <input value={recent} onChange={(e) => {
+          setRecent(e.target.value)
+        }} type="range" class="form-range w-25" id="customRange1"/>
+      </div>
+      {recent}%
+      
       <Dashboard csvData={file} />
     </div>
   );
--- src/components/InventoryInsights.js
+++ src/components/InventoryInsights.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import { PieChart, Pie, Tooltip, ResponsiveContainer } from 'recharts';
 
-const InventoryInsights = ({ data }) => (
-  <ResponsiveContainer width="100%" height={300}>
-    <PieChart>
-      <Pie dataKey="quantity" isAnimationActive={false} data={data} cx="50%" cy="50%" outerRadius={80} fill="#8884d8" label />
-      <Tooltip />
-    </PieChart>
-  </ResponsiveContainer>
-);
+const InventoryInsights = ({ data }) => {
+  const sortedData = [...data].sort((a, b) => b.quantity - a.quantity);
+  const topTenData = sortedData.slice(0, 10);
+
+  return (
+    <ResponsiveContainer width="100%" height={300}>
+      <PieChart>
+        <Pie dataKey="quantity" isAnimationActive={false} data={topTenData} cx="50%" cy="50%" outerRadius={140} fill="#8884d8" label/>
+        <Tooltip />
+      </PieChart>
+    </ResponsiveContainer>
+  );
+};
 
 export default InventoryInsights;
